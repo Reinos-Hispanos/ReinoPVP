ef48077c5af2ace6bdcd613dc34f3de7dceda002
Diegobravis20 <Diegobravis20@users.noreply.github.com>
2015-01-03 15:13:28 -0600

    Create ChatUtil.java

---
+++ scr/java/me/reinos/pvp/ChatUtil.java
@@ -0,0 +1,23 @@
+package me.reinos.pvp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+
+public class ChatUtil
+{
+private static final String PREFIX = ChatColor.BLUE + ChatColor.BOLD "[" + ChatColor.GREEN + ChatColor.BOLD + "ReinosPVP" + ChatColor.BLUE + "] " + ChatColor.WHITE
+
+
+public static void send(Player p, String message) {
+  p.sendMessage(PREFIX + message);
+}
+
+
+public static void broadcast (String message) {
+  Bukkit.broadcastMessage(PREFIX + message);
+  }
+
+
+}

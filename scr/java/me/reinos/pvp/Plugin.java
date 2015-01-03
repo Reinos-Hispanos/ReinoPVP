1be6c477289528806731c3ebe046a1fdf82193ed
BananaCoder2 <minecraftctf@hotmail.com>
2015-01-03 17:27:26 -0430

    Update Plugin.java

--- scr/java/me/reinos/pvp/Plugin.java
+++ scr/java/me/reinos/pvp/Plugin.java
@@ -1,12 +1,9 @@
 package me.reinos.pvp;
 
-import me.reinos.pvp.permissions.Permissions;
-
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.logging.Logger;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,13 +13,13 @@ public class Plugin extends JavaPlugin implements Listener{
 	public static final Logger logger = Logger.getLogger("Minecraft");
 	
 	public void onEnable() {
-		logger.info("§9§l[§a§lReinosPVP§9§l] §b§lPlugin Habilitado");
+		logger.info(ChatColor.BLUE + "[" + ChatColor.GREEN + "ReinosPVP" + ChatColor.BLUE + "]" + ChatColor.DARK_AQUA + "Plugin habilitado");
 		
 		saveConfig();
 		
 	}
 	public void onDisable() {
-		logger.info("§9§l[§a§lReinosPVP§9§l] §b§lPlugin Desabilitado");
+		logger.info(ChatColor.BLUE + "[" + ChatColor.GREEN + "ReinosPVP" + ChatColor.BLUE + "]" + ChatColor.RED + "Plugin desabilitado");
 		
 		saveConfig();
 		
@@ -32,16 +29,14 @@ public class Plugin extends JavaPlugin implements Listener{
 		String command = cmd.getName();
 		
 		if(command.equalsIgnoreCase("rpvp")) {
-		      if(player.hasPermission(Permissions.rpvp_comando)) {
 		    	  if(args.length == 0) {
-			          player.sendMessage("§4§l▃▄▅▆▇█ §a§lREINOSPVP §4§l█▇▆▅▄▃");
+			           player.sendMessage("§4§l▃▄▅▆▇█  §a§lREINOSPVP §4§l█▇▆▅▄▃");
 		                player.sendMessage("§0§l====================================");
 		                player.sendMessage("§9§l/rpvp arena §e§l- §b§lVer los comandos para crear la arena");
 		                player.sendMessage("§9§l/rpvp entrar <NOMBREARENA> §e§l- §b§lEntrar a una arena");
 		                player.sendMessage("§9§l/rpvp salir §e§l- §b§lSalir de la arena");
 		    	  }
 		      }
-		}
 		return false;
 	}
 }

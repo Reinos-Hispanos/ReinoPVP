2e31a4f25f8a1e4eb2218970bbaa6b8c1510ac80
Diegobravis20 <Diegobravis20@users.noreply.github.com>
2015-01-03 15:21:45 -0600

    Update ChatUtil.java

--- scr/java/me/reinos/pvp/ChatUtil.java
+++ scr/java/me/reinos/pvp/ChatUtil.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 
 public class ChatUtil
 {
-private static final String PREFIX = ChatColor.BLUE + ChatColor.BOLD "[" + ChatColor.GREEN + ChatColor.BOLD + "ReinosPVP" + ChatColor.BLUE + "] " + ChatColor.WHITE
+private static final String PREFIX = ChatColor.BLUE + "[" + ChatColor.GREEN + ChatColor.BOLD + "ReinosPVP" + ChatColor.BLUE + "] " + ChatColor.WHITE;
 
 
 public static void send(Player p, String message) {

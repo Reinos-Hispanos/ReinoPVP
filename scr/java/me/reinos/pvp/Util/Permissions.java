3fb896d2621eb82c619f0c3d446cea20dc7896f3
BananaCoder2 <minecraftctf@hotmail.com>
2015-01-03 18:05:06 -0430

    Create Permissions.java

---
+++ scr/java/me/reinos/pvp/Util/Permissions.java
@@ -0,0 +1,9 @@
+package me.reinos.pvp.Util;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+	
+	public static Permission rpvp_comando = new Permission("rpvp.comando");
+	
+}
